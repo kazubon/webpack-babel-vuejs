0f34222d857e9c77712b3761edb21caecd13106f
Kazuto Sato <kazubonx@gmail.com>
2020-06-03 18:42:40 +0900

    実験用コードを削除

--- src/hello.vue
+++ src/hello.vue
@@ -9,15 +9,6 @@
 </template>
 
 <script>
-function makePromise(num) {
-  return new Promise((resolve, reject) => {
-    if(num % 2 == 0)
-      resolve('OK');
-    else
-      reject('NG');
-  });
-}
-
 export default {
   data() {
     return {
@@ -26,13 +17,6 @@ export default {
   },
   methods: {
     reset() {
-      (async (a, b) => {
-        let x = await makePromise(a);
-        let y = await makePromise(b);
-        console.log(`async function finished: ${x} ${y}`);
-      })(2, 1).catch(e => {
-        console.log(`rejected ${e}`);
-      });
       this.message = 'hello';
     }
   }
